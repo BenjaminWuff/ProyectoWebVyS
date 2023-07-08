03858ff0160459453af9687e564e1fef2c2419aa
BenjaminWuff <b.paredes@duocuc.cl>
2023-07-08 17:58:26 -0400

    Empezando contacto

--- js/scripts.js
+++ js/scripts.js
@@ -7,48 +7,14 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+// Initiate the wowjs
+new WOW().init();
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
+// Sticky Navbar
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.custom-navbar').addClass('shadow-sm').css('top', '0px');
+    } else {
+        $('.custom-navbar').removeClass('navtop').css('top', '-100px');
+    }
 });
